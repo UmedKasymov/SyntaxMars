fa6b42888460aba8a658f491c26bccedb8e3f7db
[name] <umedkasymov1@gmail.com>
2020-03-17 21:12:36 -0400

    if statement added to TestOne

--- src/com/class02/TestOne.java
+++ src/com/class02/TestOne.java
@@ -5,6 +5,11 @@ public class TestOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		boolean isRaining=true;
+		if(isRaining) {
+			
+		}
+		System.out.println("it is raining");
 	}
 
 }
